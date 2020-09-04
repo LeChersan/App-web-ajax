1728ffe5eafd398652ad73b902d984447655895a
Nellychersan <nechersan@gmail.com>
2020-09-04 14:44:27 +1200

    Se creo estructura para productos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-// alert("hello")
+// $.ajax({
+//     type:"GET",
+//     dataType: "JSON",
+//     url: "data/products.json",
+//     beforeSend: function(){
+//         console.log("espere por favor...")
+
+//     },
+//     success: function(data){
+//         let productos = data
+//         // console.log(productos)
+//         console.log(productos[0].nombre)
+//         console.log(productos[1].precio)
+
+//     },
+//     error: function(){
+//         console.log("error en el servidor")
+
+//     }
+// })
+
+$.get("data/products.json", function(){
+    console.log("espere por favor")
+})
+.done(function(data){
+    console.log(data)
+    let productos = data
+    console.log(productos[0].nombre)
+    console.log( productos[1].sabores[3])
+    console.log(productos[0].sizes[2])
+})
+.fail(function(){
+    console.log("error en el servidor")
+})
+.always(function(){
+    console.log("finished")
+})
+
